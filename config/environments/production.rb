34c9e0097551ba9b002fe62490b5533ba857cd6a
ultraoligami <hazukiyokoyama0827@gmail.com>
2025-11-22 18:26:40 +0900

    Add:SNSシェア

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = {
+  host: "mvp-app-p33l.onrender.com",
+  protocol: "https"
+  }
 end
