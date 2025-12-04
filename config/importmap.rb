db38557847493ad1f7f91ff9b27b3a91787808ef
ultraoligami <hazukiyokoyama0827@gmail.com>
2025-12-04 19:49:32 +0900

    Fix: importmap bootstrap syntax error

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,15 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
+# AIユーモア JS
 pin "ai_humor"
+
+# Stimulus controllers
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Bootstrap（必須）
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+
+# Popper（Bootstrapで必要）
+pin "@popperjs/core", to: "@popperjs--core.js"
